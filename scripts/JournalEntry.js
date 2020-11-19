2c2cb18a68342b60309c3a8800218e27ff328a80
anna-moore <ammoore434@gmail.com>
2020-11-19 15:17:30 -0600

    Journal entry components displayed on DOM

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,1 +1,14 @@
 //responsible for rendering a single journal entry.
+/*
+ *  Purpose: To render a single journal entry as an
+ *           HTML representation of the data
+ */
+export const JournalEntryComponent = (entry) => {
+    return `
+        <section id="entry--${entry.id}" class="journalEntry">
+            <div class="date"> Date: ${entry.date}</div>
+            <div class="concept"> ${entry.concept}</div>
+            <div class= "text">${entry.entry}</div>
+        </section>
+    `
+}
